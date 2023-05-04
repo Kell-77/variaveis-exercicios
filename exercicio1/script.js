ad190ba2cf6c8f5c76b4fab9318df2379647645f
ezequiel <ezequielvieira.77@gmail.com>
2023-05-04 00:46:00 -0300

    comandos e variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+const nome = prompt ("seu nome completo")
+let idade = prompt ("sua idade")
+let curso = prompt ("seu curso")
+console.log (typeof nome)
+console.log (typeof idade)
+console.log (typeof curso)
+console.log ("ola ", nome, "sua idade é", idade, "e seu", curso)
+
+let pergunta = "oque estuda?"
+let pergunta2 = "oque voce ouve?"
+let pergunta3 = "oque mais?"
+
+console.log (pergunta,pergunta2,pergunta3)
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const fixo = Number(prompt("seu salario sera 3500"))
+const varaivel = Number(prompt("sua projessao 5000"))
+
+let valor = prompt ("qual seu objetivo?")
+
+console.log (typeof fixo)
+console.log (typeof varaivel)
+console.log (typeof valor)
+
+console.log("salario fixo", fixo, "mais comissoes", varaivel,"valores podem subir", valor )
+
